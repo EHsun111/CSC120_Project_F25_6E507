67d6e9accfb331b25c5346de6608de992b049f40
EHsun111 <eliasharris6@gmail.com>
2025-09-15 13:58:58 -0400

    Start of project

---
+++ main.py
@@ -0,0 +1,9 @@
+game_name = "Blasting" 
+print("Welcome to " + game_name + "!")
+print("===================") 
+
+print("Before we begin, what is your character's name?")
+name = input()
+
+
+print("Great, " + name + "! Let's begin the adventure!")
