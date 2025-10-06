b4af1160e015e8622ff8e25e082ceee2eb289466
Elias Harris <eliasharris6@gmail.com>
2025-10-06 09:13:35 -0400

    Part 2 of Project

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import random
+
 game_name = "Blasting"
-print("Welcome to " + game_name + "!\n===================")
+print(f"Welcome to {game_name}!")
+print(f"{'=' * (12 + len(game_name))}")
 
-name = input("Before we begin, what is your character's name?\n> ")
+name = input("Enter your character's name: ")
+print(f"Hello, {name}! Let's begin your journey.")
 
-print("Great, " + name + "! Let's begin the adventure!")
+player = {
+    'name': name,
+    'health': 100,
+    'coin': 0
+}
 
+events = ["find a coin", "meet a monster", "do nothing"]
+event = random.choice(events)
+print(f"While exploring, you {event}!")
+
+if event == "find a coin":
+    player['coin'] += 1
+    print(f"{player['name']} found a coin, {player['name']} now has {player['coin']} coins.")
+elif event == "meet a monster":
+    player['health'] -= 10
+    print(f"{player['name']} got hurt during the combat with monster, health is now {player['health']}.")
+elif event == "do nothing":
+    pass
